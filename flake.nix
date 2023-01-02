919e9494c110bed77dad393909a71b0fc2030e56
foldu <foldu@protonmail.com>
2023-01-02 15:57:48 +0100

    chore: Update deps

--- flake.nix
+++ flake.nix
@@ -4,25 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    naersk = {
-      url = "github:nmattia/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, naersk, flake-utils }:
+  outputs = { self, nixpkgs, crane, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.lib.${system};
         blocklistdownloadthing =
-          let
-            naersk-lib = naersk.lib."${system}".override {
-              cargo = pkgs.cargo;
-              rustc = pkgs.rustc;
-            };
-          in
-          naersk-lib.buildPackage {
-            src = ./.;
+          craneLib.buildPackage {
+            src = craneLib.cleanCargoSource ./.;
           };
       in
       {
